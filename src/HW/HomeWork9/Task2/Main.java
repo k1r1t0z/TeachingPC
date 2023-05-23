bbec4c0b2e19a622aef3b5a4af56e9ab5529cc75
Kiritoz <kiritoz@mail.ru>
2023-05-23 21:34:32 +0300

    HW9 Done two task

--- src/HW/HomeWork9/Task2/Main.java
+++ src/HW/HomeWork9/Task2/Main.java
@@ -2,14 +2,20 @@ package HW.HomeWork9.Task2;
 
 public class Main {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(15,15,15);
+        Triangle triangle = new Triangle(15, 15, 15);
         Circle circle = new Circle(10);
-        Rectangle rectangle = new Rectangle(15,10,15,10);
+        Rectangle rectangle = new Rectangle(15, 10, 15, 10);
         triangle.perimeter();
         triangle.square();
         circle.perimeter();
         circle.square();
         rectangle.perimeter();
         rectangle.square();
+        int[] array = {15, 55, 10, 40, 67};
+        int sumOfPerimeter = 0;
+        for (int i = 0; i < array.length; i++) {
+            sumOfPerimeter = sumOfPerimeter + array[i];
+        }
+        System.out.println("The sum of the perimeters of all shapes = " + sumOfPerimeter);
     }
 }
